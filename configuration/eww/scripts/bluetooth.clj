8cf3cd4c8be33d5575b4c6e9662d90baf431c7ef
Brandon Olivier <brandon@brandonolivier.com>
2025-08-06 18:33:16 -0500

    Don't hang when bluetoothctl is disabled

--- configuration/eww/scripts/bluetooth.clj
+++ configuration/eww/scripts/bluetooth.clj
@@ -1,23 +1,31 @@
 #! /usr/bin/env bb
 (ns bluetooth
-  (:require
-   [cheshire.core :as json]
-   [clojure.string :as str]
-   [babashka.process :as p]))
+  (:require [cheshire.core :as json]
+            [clojure.string :as str]
+            [babashka.process :as p]))
 
-(defn get-all-devices-info []
+(defn enabled?
+  []
+  (not (re-find #"disabled"
+                (:out (p/shell {:out :string, :continue true}
+                               "systemctl is-enabled bluetooth.service")))))
+
+(defn get-all-devices-info
+  []
   (let [get-device-data
-        (fn [line]
-          (let [[_ id] (str/split line #" " 3)
-                base (->> (p/shell {:out :string} (str "bluetoothctl info " id))
-                          :out
-                          str/split-lines
-                          (map (fn [line]
-                                 (when-let [[_ k v] (re-find #"\t(\w+): (.*)" line)]
-                                   [k v])))
-                          (remove nil?)
-                          (into {}))]
-            (assoc base "ID" id)))]
+          (fn [line]
+            (let [[_ id] (str/split line #" " 3)
+                  base (->> (p/shell {:out :string}
+                                     (str "bluetoothctl info " id))
+                            :out
+                            str/split-lines
+                            (map (fn [line]
+                                   (when-let [[_ k v] (re-find #"\t(\w+): (.*)"
+                                                               line)]
+                                     [k v])))
+                            (remove nil?)
+                            (into {}))]
+              (assoc base "ID" id)))]
     (->> (p/shell {:out :string} "bluetoothctl devices")
          :out
          str/split-lines
@@ -25,35 +33,33 @@
          (map get-device-data))))
 
 
-(defn eng->bool [word]
+(defn eng->bool
+  [word]
   (case word
     "yes" true
-    "no"  false))
+    "no" false))
 
-(defn get-bluetooth-device-data []
-  (mapv
-   (fn [device]
-     {:id (get device "ID")
-      :name (get device "Name")
-      :connected (eng->bool (get device "Connected"))})
-   (get-all-devices-info)))
+(defn get-bluetooth-device-data
+  []
+  (mapv (fn [device]
+          {:id (get device "ID"),
+           :name (get device "Name"),
+           :connected (eng->bool (get device "Connected"))})
+    (get-all-devices-info)))
 
-(defn connected? [device]
-  (= "yes" (get device "Connected")))
+(defn connected? [device] (= "yes" (get device "Connected")))
 
-(defn headset? [device]
-  (= "audio-headset" (get device "Icon")))
+(defn headset? [device] (= "audio-headset" (get device "Icon")))
 
-(do
+(if-not (enabled?)
+  (println "BT off")
   (case (first *command-line-args*)
     nil (println (json/encode (get-bluetooth-device-data)))
-
     "current" (let [device (first (filter (every-pred connected? headset?)
-                                          (get-all-devices-info)))]
-                (println (str
-                          "󰂰 "
-                          (get device "Name" "None"))))
-    "disconnect" (do (p/shell "bluetoothctl disconnect " (second *command-line-args*))
-                    (p/process "eww poll current-bluetooth-device"))
+                                    (get-all-devices-info)))]
+                (println (str "󰂰 " (get device "Name" "None"))))
+    "disconnect" (do (p/shell "bluetoothctl disconnect "
+                              (second *command-line-args*))
+                     (p/process "eww poll current-bluetooth-device"))
     "connect" (do (p/shell "bluetoothctl connect " (second *command-line-args*))
                   (p/process "eww poll current-bluetooth-device"))))
