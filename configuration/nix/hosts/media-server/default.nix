ce0b68ed5c1d4dbc3e96c78d92c94b33f7f12ccd
Brandon Olivier <brandon@brandonolivier.com>
2026-08-04 15:38:32 -0500

    feat: run plex behind nginx on the media-server host

--- configuration/nix/hosts/media-server/default.nix
+++ configuration/nix/hosts/media-server/default.nix
@@ -25,5 +25,35 @@
     nssmdns4 = true;
   };
 
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  # Serve the web UI at port 80; Plex apps still use 32400
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts."media-server.local" = {
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:32400";
+        proxyWebsockets = true;
+      };
+    };
+  };
+  networking.firewall.allowedTCPPorts = [ 80 ];
+
+  # Media library at /srv/media, group-writable so brandon can add files
+  systemd.tmpfiles.rules = [
+    "d /srv/media 2775 plex plex -"
+  ];
+  users.users.brandon.extraGroups = [ "plex" ];
+
+  # VA-API for hardware transcoding (needs Plex Pass)
+  hardware.graphics = {
+    enable = true;
+    extraPackages = [ pkgs.intel-media-driver ];
+  };
+
   system.stateVersion = "25.11";
 }
